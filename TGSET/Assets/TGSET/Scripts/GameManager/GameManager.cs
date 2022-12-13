5eabf19a30af063f900dd6970b6e4cf662ddb2aa
Chrd26 <christosdouris@outlook.com>
2022-12-13 22:02:34 +0200

    Added Animations, Changed the book so it works with bones, Unity doesn't support blender modifiers

---
+++ TGSET/Assets/TGSET/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager managerInstance;
+    private bool isOnLevel1, isOnLevel2, isOnLevel3, isOnLevel4, isOnLevel5, areControllsEnabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initialize Defaults
+        isOnLevel1 = false;
+        isOnLevel2 = false;
+        isOnLevel3 = false;
+        isOnLevel4 = false;
+        isOnLevel5 = false;
+        areControllsEnabled = false;
+
+        //Set the instance of the managerInstance to this
+        if (managerInstance == null)
+        {
+            managerInstance = this;
+        }
+        
+    }
+}
