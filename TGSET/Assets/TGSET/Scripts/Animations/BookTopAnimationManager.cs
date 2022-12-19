f9583b4717ee6d4c04ef0e87c0a58d4c7f69ab42
Chrd26 <christosdouris@outlook.com>
2022-12-19 22:11:47 +0200

    Added start game logic, opening book animation now plays when the game starts, added materials for each page to accommodate camera textures

---
+++ TGSET/Assets/TGSET/Scripts/Animations/BookTopAnimationManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookTopAnimationManager : MonoBehaviour
+{
+    //Create an animation variable
+    private Animation openBookAnim;
+    public GameObject startButton;
+    public Animation spineBookOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set variable to the animation component
+        openBookAnim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Play Animation Function
+    public void PlayOpenBookAnimation() {
+
+        openBookAnim.Play("BookTopAnim");
+        spineBookOpen.Play("BookSpineOpeningAnim");
+        startButton.SetActive(false);
+    }
+}
