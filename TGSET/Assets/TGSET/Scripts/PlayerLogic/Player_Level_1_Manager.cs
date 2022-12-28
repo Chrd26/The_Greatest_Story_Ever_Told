a2bb685c5c42c7772c01d930b53e4f876c9f86b5
Chrd26 <christosdouris@outlook.com>
2022-12-28 18:07:55 +0200

    Added animations in animator states, made the player movable with WASD buttons

--- TGSET/Assets/TGSET/Scripts/PlayerLogic/Player_Level_1_Manager.cs
+++ TGSET/Assets/TGSET/Scripts/PlayerLogic/Player_Level_1_Manager.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Player_Level_1_Manager : MonoBehaviour
 {
     private Animator playerAnimations;
+    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         playerAnimations = GetComponent<Animator>();
         playerAnimations.SetTrigger("FrontWalkAnim");
+        speed = 10.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         AnimationManager();
+        movement();
     }
 
     private void AnimationManager()
@@ -24,6 +27,10 @@ public class Player_Level_1_Manager : MonoBehaviour
 
     }
 
+    private void movement()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0);
+    }
 
 
 }
