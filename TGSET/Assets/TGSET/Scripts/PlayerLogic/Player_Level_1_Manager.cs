b1b22acc8500f64f4f68fa75cd9d03b272f1c832
Chrd26 <christosdouris@outlook.com>
2022-12-28 22:20:31 +0200

    Implemented Side Animations, Changed the name of the attack animations to spell animations

--- TGSET/Assets/TGSET/Scripts/PlayerLogic/Player_Level_1_Manager.cs
+++ TGSET/Assets/TGSET/Scripts/PlayerLogic/Player_Level_1_Manager.cs
@@ -11,7 +11,6 @@ public class Player_Level_1_Manager : MonoBehaviour
     void Start()
     {
         playerAnimations = GetComponent<Animator>();
-        playerAnimations.SetTrigger("FrontWalkAnim");
         speed = 10.0f;
     }
 
@@ -22,10 +21,106 @@ public class Player_Level_1_Manager : MonoBehaviour
         movement();
     }
 
+    //Manage Animations Based on Key Pressess
+
     private void AnimationManager()
     {
-
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlayWalkLeftSideAnimation();
+            StopIdleLeftSideAnimation();
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            PlayWalkRightSideAnimation();
+            StopIdleRightSideAnimation();
+        }
+        
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            {
+                PlayIdleLeftSideAnimation();
+                StopWalkLeftSideAnimation();
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            {
+                PlayIdleRightSideAnimation();
+                StopWalkRightSideAnimation();
+            }
+        }
+        
     }
+
+    //Left Side Animations Functions
+
+    private void PlayWalkLeftSideAnimation()
+    {
+        playerAnimations.SetTrigger("WalkLeftAnim");
+    }
+
+    private void PlayIdleLeftSideAnimation()
+    {
+        playerAnimations.SetTrigger("IdleLeftAnim");
+    }
+
+    private void PlaySpellLeftSideAnimation()
+    {
+        playerAnimations.SetTrigger("SpellLeftAnim");
+        playerAnimations.ResetTrigger("WalkRightAnim");
+    }
+
+    private void StopWalkLeftSideAnimation()
+    {
+        playerAnimations.ResetTrigger("WalkLeftAnim");
+    }
+
+    private void StopIdleLeftSideAnimation()
+    {
+        playerAnimations.ResetTrigger("IdleLeftAnim");
+    }
+
+    private void StopAttackLeftSideAnimation()
+    {
+        playerAnimations.ResetTrigger("SpellLeftAnim");
+    }
+
+    //Right Side Animations Functions
+
+    private void PlayWalkRightSideAnimation()
+    {
+        playerAnimations.SetTrigger("WalkRightAnim");
+        playerAnimations.ResetTrigger("WalkLeftAnim");
+    }
+
+    private void PlayIdleRightSideAnimation()
+    {
+        playerAnimations.SetTrigger("IdleRightAnim");
+    }
+
+    private void PlaySpellRightSideAnimation()
+    {
+        playerAnimations.SetTrigger("SpellRightAnim");
+    }
+
+    private void StopWalkRightSideAnimation()
+    {
+        playerAnimations.ResetTrigger("WalkRightAnim");
+    }
+
+    private void StopIdleRightSideAnimation()
+    {
+        playerAnimations.ResetTrigger("IdleRightAnim");
+    }
+
+    private void StopAttackrightSideAnimation()
+    {
+        playerAnimations.ResetTrigger("SpellRightAnim");
+    }
+
 
     private void movement()
     {
