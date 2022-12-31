5b42f4963a15af25d406c4d3d4625391185b033c
Chrd26 <christosdouris@outlook.com>
2022-12-31 17:23:05 +0200

    Added First and Final pages for the jam game

---
+++ TGSET/Assets/TGSET/Scripts/Animations/ParticleManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    public ParticleSystem particle;
+    public Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.managerInstance.isFreezeTimeCasted)
+        {
+            particle.Pause();
+            material.SetColor("_EmissiveColor", Color.cyan * 100);
+        }
+        else
+        {
+            particle.Play();
+            material.SetColor("_EmissiveColor", Color.yellow * 100);
+        }
+        
+    }
+}
